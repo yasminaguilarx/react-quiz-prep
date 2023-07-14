0e790335f7bc36210f055bc6205b81001aa9c6da
Garrett Ross <garrettross@gGarrettRoss.attlocal.net>
2023-07-14 09:41:01 -0500

    Quiz

---
+++ src/App.jsx
@@ -0,0 +1,24 @@
+
+
+
+function App() {
+  
+
+  // Instructions:
+    // 1. Use a react hook to create some state called users with an initial value of an empty Array.
+    // 2. Use the endpoint https://jsonplaceholder.typicode.com/users to get data
+    // 3. Use a hook to make the API request when the app loads. 
+    // 4. Create Users and UserItem components
+    // 5. Pass down the users state into the Users component, loop through it, and render UserItem components
+    // 6. Add event listeners to each UserItem, that when clicked will console.log() that UserItems unique ID
+
+
+
+  return (
+    <div className="container">
+      <h1>See instructions commented out in App.jsx!</h1>
+    </div>
+  )
+}
+
+export default App
